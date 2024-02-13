9e4befef62d2e2495f71f37d9982936db11bda8d
Ashritha-19 <ashri.vig2337@gmail.com>
2024-02-13 12:33:50 +0530

    abc

--- Bank.java
+++ Bank.java
@@ -1,9 +1,12 @@
 public class Bank {
     public String accountNo;
-    public String futurebranch(){
+    public String futurebranch() {
         return "stash";
     }
     public String FutureBranch2() {
  return "FutureBranch2";
+    }
+    public String krish(){
+        return "krish";
     }
 }
